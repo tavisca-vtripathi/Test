1fe988d2d401593886fba238cfa8dad1975b2fb5
vtripathi <vtripathi@tavisca.com>
2015-07-04 18:21:57 +0530

    This is first commit for overloading assignment

---
+++ Over_Loading/Over_Loading/Distance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Over_Loading
+{
+    class Distance
+    {
+        int km, m;
+        public Distance()
+        { 
+        }
+        public Distance(int km, int m)
+        {
+            this.km = km;
+            this.m = m;
+        
+        }
+        public void disp()
+        {
+            Console.WriteLine("Km={0}", km);
+            Console.WriteLine("m={0}",m);
+
+        }
+        public static Distance operator +(Distance l1, Distance l2)
+        {
+            Distance l3 = new Distance();
+            l3.km = l1.km + l2.km;
+            l3.m = l1.m + l2.m;
+            if (l3.m >= 1000)
+            {
+                l3.km++;
+                l3.m = l3.m - 1000;
+            }
+            return l3;
+        }
+    }
+}
---
+++ Over_Loading/Over_Loading/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Over_Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Distance atb = new Distance(245, 700);
+            Distance btc = new Distance(248,500);
+            Distance atc = atb + btc;
+            atc.disp();
+        }
+    }
+}
